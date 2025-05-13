4898a02d943ab13099eb5364da5e5f5c44dd4fab
Yeray-Dev <wow_ybf@hotmail.es>
2025-05-13 13:12:14 +0200

    RPS/UI - Start

--- Rock_Paper_Scissors/RPS.js
+++ Rock_Paper_Scissors/RPS.js
@@ -1,46 +1,37 @@
-function getHumanChoice(){
-	const date = prompt("Rock, Paper o Scissors");
-	if (date == "Rock"){
-		console.log("You choice: Rock")
-		return 0
-	}else if (date == "Paper"){
-		console.log("You choice: Paper")
-		return 1
-	}else if (date == "Scissors"){
-		console.log("You choice: Scissors")
-		return 2
-	}else 
-		getHumanChoice()
-}
-function getComputerChoice(){
-	let date = Math.floor(Math.random() * (2 - 0 + 1) + 0)
-	if (date == 0){
-		console.log("The Computer choice: Rock")
-		return 0
-	}else if (date == 1){
-		console.log("The Computer choice: Rock")
-		return 1
-	}else if (date == 2){
-		console.log("The Computer choice: Rock")
-		return 2
-	}
-}
-function playRound(human, computer){
 
-	if (human == computer)
+function playRound(human){
+	let human_score = 0
+	let computer_score = 0
+	let computer = Math.floor(Math.random() * (2 - 0 + 1) + 0)
+	if (human == computer){
 		console.log("Draw")
+		alert("Draw")
+	}
 	//. Humman win
-	else if (human == 0 && computer == 2 || human == 2 && computer == 1 || human == 1 && computer == 0){
-
-		console.log("You win!!")
+	else if (human == 0 && computer == 2 || human == 2 && computer == 1 
+		|| human == 1 && computer == 0){
+			console.log("You win!!")
+			human_score++
+		}
+		//. Computer win
+		else if (human == 2 && computer == 0 || human == 1 && computer == 0 
+			|| human == 0 && computer == 1){
+				console.log("Computer win!!")
+				computer_score++
+			}
+			
+			if (human_score > computer_score)
+				alert("You win!!")
+			else if (computer_score > human_score)
+				alert("Computer win!!")
+			else
+			alert("Draw")
 	}
 
-	//. Computer win
-	else if (human == 2 && computer == 0 || human == 1 && computer == 0 || human == 0 && computer == 1){
-		console.log("Computer win!!")
-	}
-}
+const buttons = document.querySelectorAll("button")
 
-let human = getHumanChoice()
-let computer = getComputerChoice()
-playRound(human, computer)
+buttons.forEach((button) => {
+	button.addEventListener("click", () => {
+		playRound(button.id)
+	})
+})

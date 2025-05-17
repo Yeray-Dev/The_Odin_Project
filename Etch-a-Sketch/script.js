3eeb5b21583bf98756a06c3fdfa25a3c5c61fdc6
YerayBlanco <wow_ybf@hotmail.es>
2025-05-17 11:28:39 +0200

    EaS / Finish

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -2,7 +2,7 @@ function colorGenerate(){
 	return "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0");
 }
 
-let numberOfGrid = prompt("Numero de casillas: ")
+let numberOfGrid = prompt("Numero de casillas (1 - 100): ")
 numberOfGrid = parseInt(numberOfGrid)
 
 let size = 100 / numberOfGrid
@@ -27,6 +27,10 @@ for (let i = 0; i < numberOfGrid * numberOfGrid; i++){
 	container.appendChild(newDiv)
 }
 
+const startButton = document.querySelector("button")
+startButton.addEventListener("click", () => {
+	location.reload()
+} )
 
 
 document.body.appendChild(container);

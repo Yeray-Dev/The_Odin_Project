612c80e312a7a7a3aeb65cc75df592315cc2d3d2
Yeray-Dev <wow_ybf@hotmail.es>
2025-05-20 13:01:34 +0200

    CALCULATOR - CSS Done

---
+++ Calculator/calculator.js
@@ -0,0 +1,10 @@
+function operations(num1, num2, symbol){
+	if (symbol == '+')
+		return num1 + num2
+	else if (symbol == '-')
+		return num1 - num2
+	else if (symbol == '*')
+		return num1 * num2
+	else if (symbol == '%')
+		return num1 / num2
+}
